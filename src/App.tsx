f378063fdf550b28c51402b14e605cc40098fe08
Volcomix <Volcomix@users.noreply.github.com>
2022-01-16 19:31:52 +0100

    Show lighting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { FreeCamera } from '@babylonjs/core/Cameras/freeCamera'
 import { Engine } from '@babylonjs/core/Engines/engine'
 import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight'
+import '@babylonjs/core/Materials/standardMaterial'
 import { Vector3 } from '@babylonjs/core/Maths/math'
 import { Mesh } from '@babylonjs/core/Meshes/mesh'
 import '@babylonjs/core/Meshes/meshBuilder'
@@ -25,23 +26,19 @@ export default function App() {
           new Vector3(0, 1, 0),
           scene,
         )
-        light.intensity = 1000
+        light.intensity = 0.7
 
         const material = new GridMaterial('grid', scene)
+
         const sphere = Mesh.CreateSphere('sphere1', 16, 2, scene)
         sphere.position.y = 2
-        sphere.material = material
 
         const ground = Mesh.CreateGround('ground1', 6, 6, 2, scene)
         ground.material = material
 
-        engine.runRenderLoop(function () {
-          scene.render()
-        })
+        engine.runRenderLoop(() => scene.render())
 
-        window.addEventListener('resize', function () {
-          engine.resize()
-        })
+        window.addEventListener('resize', () => engine.resize())
       }}
       className="App-canvas"
     />
